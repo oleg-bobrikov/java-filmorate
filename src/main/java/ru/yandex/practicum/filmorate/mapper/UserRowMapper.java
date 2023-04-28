67152a66b61af37cc09d0ae985da0698e8fb1bba
Oleg Bobrikov <bobrikov@yandex.ru>
2023-04-29 00:26:54 +0300

    refactor: UserRowMapper

--- src/main/java/ru/yandex/practicum/filmorate/mapper/UserRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/UserRowMapper.java
@@ -7,6 +7,7 @@ import ru.yandex.practicum.filmorate.dto.User;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Objects;
+
 @Component
 public class UserRowMapper implements RowMapper<User> {
 
