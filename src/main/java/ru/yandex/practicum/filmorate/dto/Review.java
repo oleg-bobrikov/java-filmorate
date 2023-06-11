b85d8254cbfe6014845db0d59f74a1ca27b79489
Oleg Bobrikov <bobrikov@yandex.ru>
2023-06-12 00:56:44 +0300

    feature: DataBaseException fix: FilmController fix: FilmH2Storage fix: FilmStorageTest fix: GenreStorageTest fix: MpaStorageTest feat: Review feat: ReviewController feat: ReviewH2Storage feat: ReviewLike feat: ReviewRowMapper feat: ReviewService fix: UserService fix: UserStorageTest

---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/Review.java
@@ -0,0 +1,44 @@
+package ru.yandex.practicum.filmorate.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@Builder
+public class Review {
+
+    private Integer reviewId;
+
+    @NotBlank(message = "Content cannot be empty")
+    private String content;
+
+    @NotNull
+    private Boolean isPositive;
+
+    @NotNull
+    private Integer userId;
+
+    @NotNull
+    private Integer filmId;
+
+    private int useful;
+
+    public Map<String, Object> toMap() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("ID", reviewId);
+        params.put("CONTENT", content);
+        params.put("IS_POSITIVE", isPositive);
+        params.put("USER_ID", userId);
+        params.put("FILM_ID", filmId);
+        params.put("USEFUL", useful);
+        return params;
+    }
+
+
+
+}
