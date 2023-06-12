64e6d092d28501018338d6d6eb929c85c2a6f9de
Oleg Bobrikov <bobrikov@yandex.ru>
2023-06-12 12:51:16 +0300

    codestyle: GenreRowMapper

--- src/main/java/ru/yandex/practicum/filmorate/mapper/GenreRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/GenreRowMapper.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.dto.Genre;
-import ru.yandex.practicum.filmorate.storage.GenreStorage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
