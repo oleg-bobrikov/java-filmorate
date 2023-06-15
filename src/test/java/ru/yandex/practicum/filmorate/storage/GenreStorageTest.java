421995826cc8decf02f2a497a40d4a79a723f5db
Oleg Bobrikov <bobrikov@yandex.ru>
2023-06-15 23:40:53 +0300

    refactor

--- src/test/java/ru/yandex/practicum/filmorate/storage/GenreStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/GenreStorageTest.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
